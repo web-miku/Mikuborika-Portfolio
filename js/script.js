67ec49de18a0b860b524bcffa2419e9588f6a45a
web-miku <143951060+web-miku@users.noreply.github.com>
2023-09-22 11:05:35 +0900

    initial commit.
    最初のアップです。2023/09/22   11:05

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+$(function(){
+  var pagetop = $('#page-top');
+  pagetop.hide();
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+  });  });
